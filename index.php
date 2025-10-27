69f5326dca567111dee1dc1cc84fd7c851e046e9
MichaelPain <cattiveria89@gmail.com>
2025-10-27 21:13:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ require_once __DIR__ . '/includes/bootstrap.php';
         <li><a href="/pages/tende/index.php">Gestione Tende</a></li>
         <li><a href="/pages/attrezzatura/index.php">Gestione Attrezzatura</a></li>
         <li><a href="/pages/ordini/index.php">Gestione Ordini</a></li>
+        <li><a href="/pages/veicoli/index.php">Gestione Veicoli</a></li>
+        <li><a href="/pages/operai/index.php">Gestione Operai</a></li>
+        <li><a href="/pages/assegnazioni/index.php">Gestione Assegnazioni</a></li>
     </ul>
 </main>
 <?php include __DIR__ . '/includes/footer.php'; ?>
