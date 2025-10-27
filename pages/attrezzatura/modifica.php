2b63763da87d61abf9710dce652437bfa1252735
MichaelPain <cattiveria89@gmail.com>
2025-10-27 20:18:12 +0100

    Update modifica.php

--- pages/attrezzatura/modifica.php
+++ pages/attrezzatura/modifica.php
@@ -1,8 +1,5 @@
 <?php
-// pages/attrezzatura/modifica.php
-require_once __DIR__ . '/../../includes/db.php';
-require_once __DIR__ . '/../../includes/helpers.php';
-require_once __DIR__ . '/../../includes/auth.php';
+require_once __DIR__ . '/../../includes/bootstrap.php';
 require_login();
 
 $id = (int)($_GET['id'] ?? 0);
