0f7a32dd267c9a19fe1d2f5346323c1a54c57b70
MichaelPain <cattiveria89@gmail.com>
2025-10-27 20:13:53 +0100

    Update includes to use bootstrap.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . '/includes/auth.php';
-require_once __DIR__ . '/includes/db.php';
+require_once __DIR__ . '/includes/bootstrap.php';
 ?>
 <?php include __DIR__ . '/includes/header.php'; ?>
 <main class="container">
