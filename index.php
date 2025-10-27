b4b1ade6997b0b588374ce202883c8f70e209c1c
MichaelPain <cattiveria89@gmail.com>
2025-10-27 20:40:19 +0100

    Update links to point to the correct paths

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ require_once __DIR__ . '/includes/bootstrap.php';
     <h1>Dashboard</h1>
     <p>Benvenuto nel Gestionale Tende.</p>
     <ul>
-        <li><a href="/gestionaletende/pages/clienti/index.php">Gestione Clienti</a></li>
-        <li><a href="/gestionaletende/pages/tende/index.php">Gestione Tende</a></li>
-        <li><a href="/gestionaletende/pages/attrezzatura/index.php">Gestione Attrezzatura</a></li>
-        <li><a href="/gestionaletende/pages/ordini/index.php">Gestione Ordini</a></li>
+        <li><a href="/pages/clienti/index.php">Gestione Clienti</a></li>
+        <li><a href="/pages/tende/index.php">Gestione Tende</a></li>
+        <li><a href="/pages/attrezzatura/index.php">Gestione Attrezzatura</a></li>
+        <li><a href="/pages/ordini/index.php">Gestione Ordini</a></li>
     </ul>
 </main>
 <?php include __DIR__ . '/includes/footer.php'; ?>
