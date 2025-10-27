624ea85023564462732df47ff11c1c1f04b90e74
MichaelPain <cattiveria89@gmail.com>
2025-10-27 20:14:24 +0100

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,5 @@
 <?php
-require_once __DIR__ . '/../includes/db.php';
-require_once __DIR__ . '/../includes/helpers.php';
-require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/bootstrap.php';
 
 if (current_user()) {
     redirect('/index.php');
