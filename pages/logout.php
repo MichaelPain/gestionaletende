34dc5ec10f5081dd05b97cfb0adffe16a107fd2a
MichaelPain <cattiveria89@gmail.com>
2025-11-02 20:43:20 +0100

    Delete pages directory

--- pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/bootstrap.php';
-$_SESSION = [];
-if (session_status() === PHP_SESSION_ACTIVE) {
-    session_destroy();
-}
-header('Location: /pages/login.php');
-exit;
